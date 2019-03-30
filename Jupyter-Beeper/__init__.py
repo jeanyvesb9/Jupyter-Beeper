656f283567b0f760f1f47aa2cfab18cbd2bfd0ca
Jean Yves Beaucamp <jeanyvesb9@me.com>
2019-03-29 21:43:06 -0300

    V1.0.1
    - Fixed wrong __init__.py import
    - Added long_description in setup.py

---
+++ Jupyter-Beeper/__init__.py
@@ -0,0 +1,1 @@
+from .Beeper import *
--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 import setuptools
 from distutils.core import setup
+from os import path 
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='Jupyter-Beeper',
-    version='1.0',
+    version='1.0.1',
     author="Jean Yves Beaucamp (@jeanyvesb9)",
     author_email="jeanyvesb9@me.com",
-    description="A beep generator for Jupyter Notebooks (and Jupyter-Lab) that doesn't display a reproduction widget.",
+    description="A beep generator for Jupyter Notebooks (and Jupyter-Lab) that doesn't display an audio reproduction widget.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/jeanyvesb9/Jupyter-Beeper",
-    packages=['Beeper'],
+    packages=['Jupyter-Beeper'],
     keywords='beep beeper sounds ipython jupyter',
     install_requires=['ipython', 'numpy'],
 )
